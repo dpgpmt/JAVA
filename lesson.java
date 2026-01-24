8232cc89f5b84f5e881f5609dce8449e8daa1b2f
Paramjeet-pmt <231895267+Paramjeet-pmt@users.noreply.github.com>
2026-01-24 13:10:03 +0530

    Program to find area of circle by taking radius as input from User

---
+++ lesson.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class lesson {
+    public static void main(String[] args) {
+        int aga = 19;
+        System.out.println(aga);
+
+        double marks = 89.80;
+        System.out.println(marks);
+
+        boolean isPass = true;
+        System.out.println(isPass);
+
+        //Take Radius as input from user and give the area of the circle
+        // Scanner sc = new Scanner(System.in);
+        // Integer Radius = sc.nextInt();
+        // System.out.println("The Radius of circle is "+Radius);
+        // double area = (3.14*Radius*Radius);
+
+        // System.out.println("The Area of the circle is "+area);
+
+        //For Printing the Table of the give Number;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number Whoose Table you want to print");
+        Integer Number = sc.nextInt();
+
+        System.out.println(Number+ "*1=  "+Number*1);
+        System.out.println(Number+ "*2=  "+Number*2);
+        System.out.println(Number+ "*3=  "+Number*3);
+        System.out.println(Number+ "*4=  "+Number*4);
+        System.out.println(Number+ "*5=  "+Number*5);
+        System.out.println(Number+ "*6=  "+Number*6);
+        System.out.println(Number+ "*7=  "+Number*7);
+        System.out.println(Number+ "*8=  "+Number*8);
+        System.out.println(Number+ "*9=  "+Number*9);
+        System.out.println(Number+ "*10= "+Number*10);
+
+
+
+        
+    }
+
+}
